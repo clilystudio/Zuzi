40d620592e871d71aa9805b91e7d1e2914e5659b
clilystudio <bullsgl@gmail.com>
2016-07-25 17:38:59 +0800

    MainActivity

--- workspace/src/main/java/com/clilystudio/zuzi/Main.java
+++ workspace/src/main/java/com/clilystudio/zuzi/Main.java
@@ -1,8 +1,26 @@
 package com.clilystudio.zuzi;
 
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
 public class Main {
 
 	public static void main(String[] args) {
+		String apkFile = "E:/zssq-Official.apk";
+		try {
+			ZipFile zipFile = new ZipFile(apkFile);
+			Enumeration<? extends ZipEntry> entries = zipFile.entries();
+			while (entries.hasMoreElements()) {
+				ZipEntry element = entries.nextElement();
+				System.out.println("element:" + element.getName());
+			}
+			zipFile.close();
+		} catch (IOException e) {
+	        System.err.println("Can't open file:" + apkFile);
+			e.printStackTrace();
+		}
         System.out.println("Zuzi.");
 	}
 
